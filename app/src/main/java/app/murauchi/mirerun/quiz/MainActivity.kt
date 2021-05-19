4d44b1d8347f6b96a3164716ea8cea0a8d3b9752
mirei <mirei.saxophone1010@gmail.com>
2021-05-19 16:18:49 +0900

    クイズアプリ(クイズ画面)完成

--- app/src/main/java/app/murauchi/mirerun/quiz/MainActivity.kt
+++ app/src/main/java/app/murauchi/mirerun/quiz/MainActivity.kt
@@ -3,12 +3,15 @@ package app.murauchi.mirerun.quiz
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Log.d("debug", "みれるん")
 
         //クイズ画面に遷移する準備をする
         val quizIntent: Intent = Intent(this, QuizActivity::class.java)
--- app/src/main/java/app/murauchi/mirerun/quiz/QuizActivity.kt
+++ app/src/main/java/app/murauchi/mirerun/quiz/QuizActivity.kt
@@ -2,10 +2,116 @@ package app.murauchi.mirerun.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_quiz.*
 
 class QuizActivity : AppCompatActivity() {
+    //クイズを作る
+    val quizLists: List<List<String>> = listOf(
+        listOf("Androidコースのキャラクターの名前は？","ランディ","フィル","ドロイド","ランディ"),
+        listOf("Androidアプリを開発する言語はどれ？","JavaScript","Kotlin","Swift","Kotlin"),
+        listOf("ImageViewは、何を扱える要素？","文字","音声","画像","画像")
+    )
+    //クイズをシャッフルして変数に入れる
+    val shuffledLists: List<List<String>> = quizLists.shuffled()
+    //クイズ数をカウントする変数を作る
+    var quizCount: Int = 0
+    //正解の答えを入れる変数を作る
+    var correctAnswer: String = ""
+    //正解の回数を入れる変数を作る
+    var correctCount: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+        //関数を呼び出してクイズを表示
+        showQuestion()
+
+        //1つ目のボタンがタッチされたら
+        answerButton1.setOnClickListener {
+            //解答をチェック
+            checkAnswer(answerButton1.text.toString())
+        }
+        answerButton2.setOnClickListener {
+            //解答をチェック
+            checkAnswer(answerButton2.text.toString())
+        }
+        answerButton3.setOnClickListener {
+            //解答をチェック
+            checkAnswer(answerButton3.text.toString())
+        }
+        //次に進むボタンがタップされたら
+        nextButton.setOnClickListener {
+            //現在のクイズ数と全問クイズ数が一致するか比較して
+            if (quizCount == quizLists.size) {
+                //一緒なら結果画面へ移動する準備
+                //クイズ数をセット
+                //正解数をセット
+                //結果画面に移動
+
+            } else {
+                //一緒でなければ判定●×画像を非表示にする
+                judgeImage.isVisible = false
+                nextButton.isVisible = false
+                //解答ボタンの有効にする
+                answerButton1.isEnabled = true
+                answerButton2.isEnabled = true
+                answerButton3.isEnabled = true
+                //正解表示をリセットする
+                correctAnswerText.text = ""
+                //クイズを表示する
+                showQuestion()
+
+            }
+        }
+    }
+
+    //画面に表示するクイズを作る
+    fun showQuestion() {
+        //クイズを取り出す
+        val question: List<String> = shuffledLists[quizCount]
+        //クイズの中身を確認する
+        //Log.d("debug", question.toString())
+        //クイズをTextViewに反映する
+        quizText.text = question[0]
+        //クイズの選択肢を表示する
+        answerButton1.text = question[1]
+        answerButton2.text = question[2]
+        answerButton3.text = question[3]
+        //クイズの正しい答えをリセット
+        correctAnswer = question[4]
+    }
+
+    //解答をチェック
+    fun checkAnswer(answerText: String) {
+        //タップされた解答と正しい解答を比べて
+        if (answerText == correctAnswer) {
+            //一緒だったら〇画像を反映
+            judgeImage.setImageResource(R.drawable.maru_image)
+            //正解した回数をカウント
+            correctCount++
+        } else {
+            //違っていたら×画像を反映
+            judgeImage.setImageResource(R.drawable.batu_image)
+        }
+        //判定画像を表示する
+        showAnswer()
+        //クイズ数をカウント
+        quizCount++
+    }
+
+    //判定画像を表示する際のボタンを設定
+    fun showAnswer() {
+        //正解を表示する
+        correctAnswerText.text = "正解：$correctAnswer"
+        //判定画像を表示(●×)
+        judgeImage.isVisible = true
+        //次へボタンを表示
+        nextButton.isVisible = true
+        //解答ボタンを無効にする
+        answerButton1.isEnabled = false
+        answerButton2.isEnabled = false
+        answerButton3.isEnabled = false
     }
 }
